328e0bc35cf60299580111187fe606a044c20387
KPaezAstudillo <karinapaezastudillo@gmail.com>
2022-09-05 21:00:04 +0000

    add button

--- src/app.js
+++ src/app.js
@@ -1,5 +1,38 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  let button = document.getElementById("clickme");
+  button.addEventListener("click", function() {
+    let Valor = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+    ];
+    let Pinta = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
+    let sortValor = Valor[Math.floor(Math.random() * Valor.length)];
+    let sortPinta = Pinta[Math.floor(Math.random() * Pinta.length)];
 
-window.onload = function() {};
+    let color =
+      sortPinta == "&hearts;" || sortPinta == "&diams;" ? "red" : "black";
+
+    let top = document.getElementById("top");
+    let number = document.getElementById("number");
+    let bottom = document.getElementById("bottom");
+
+    top.style.color = color;
+    number.style.color = color;
+    bottom.style.color = color;
+
+    top.innerHTML = sortPinta;
+    number.innerHTML = sortValor;
+    bottom.innerHTML = sortPinta;
+  });
+};
